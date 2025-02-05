8fc93f2aaa332d1d59d7d4f9e37d77f4c740b0ff
Juan Victor <juanvictorqwerty@gmail.com>
2025-02-05 13:18:19 +0100

    applicants are now recorded

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
-    include("database.php");
+ include ("database.php");
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form</title>
+</head>
+<body>
+    <h2>Personal Information Form</h2>
+    <form action="applicants.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
 
-    $sql = "INSERT INTO demands (  applicant_id, program_code, admission_status )
-            VALUES( '1','2','3' )";
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
 
-    if (mysqli_query($connection, $sql)) {
-        echo "Query submitted successfully.";
-    } else {
-        echo "Error submitting query: " . mysqli_error($connection);
-    }
+        <label for="phone">Phone:</label>
+        <input type="phone" id="phone" name="phone" required><br><br>
 
-    mysqli_close($connection);
-?>
+        <label for="birthday">Birthday:</label>
+        <input type="date" id="birthday" name="birthday" required><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
